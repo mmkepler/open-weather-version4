147afa3426a0807b69ddf30690b4fd1e588e5a14
M. Kepler <mmkepler@yahoo.com>
2025-11-05 19:57:05 -0700

    fix: footer redo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,15 @@ import gitHub from "../assets/GitHub-Mark-32px.png"
 
 export default function footer() {
   return (
-    <footer>
-      footer
+    <footer id="footer">
+      <p>
+        <a href="https://www.mmkepler.com" rel="noopener noreferrer" target="_blank">Created by Melissa Kepler</a>
+      </p>
+      <p>
+        <a href="https://github.com/mmkepler/open-weather-version4" rel="noopener noreferrer" target="_blank">
+          <img src={gitHub}/>
+        </a>
+      </p>
     </footer>
   )
 }
